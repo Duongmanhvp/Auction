3f0dc539b994ba00c800ed9805e5e06cea5bb5fb
Trần Minh Sáng <92042025+achibulup@users.noreply.github.com>
2024-07-23 04:01:49 +0000

    delete folder for refactor, will re add later

--- back_end/src/main/java/com/ghtk/auction/exception/EmailException.java
+++
@@ -1,7 +0,0 @@
-package com.ghtk.auction.exception;
-
-public class EmailException extends RuntimeException {
-    public EmailException(String otpServiceUnavailable, Exception e) {
-        super(otpServiceUnavailable, e);
-    }
-}
